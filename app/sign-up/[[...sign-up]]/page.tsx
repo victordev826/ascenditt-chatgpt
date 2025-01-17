165228b5fa5f9d32c77683f305e0960341e44579
victordev826 <devictorfreitas@gmail.com>
2025-01-16 20:44:01 -0600

    changed aftersignupurl

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="flex justify-center py-24">
-      <SignUp signInUrl="/sign-in"/>
+      <SignUp signInUrl="/sign-in" afterSignUpUrl='/chat'/>
     </div>
   );
 }
